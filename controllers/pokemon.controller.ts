e3cf2a44d59f28491cfabca139cd45bae6498e0f
QuentinR <rouvilleq@gmail.com>
2021-04-07 10:53:31 +0200

    When both pokemons have the same speed, the winner will be random

--- controllers/pokemon.controller.ts
+++ controllers/pokemon.controller.ts
@@ -3,7 +3,16 @@ export class PokemonController
 {
     static whichAttackFirst(pokemon1: Pokemon, pokemon2: Pokemon): Pokemon
     {
-        if(pokemon1.speed > pokemon2.speed){
+        if(pokemon1.speed === pokemon2.speed)
+        {
+            if(Math.floor(Math.random()) === 0) {
+                return pokemon1;
+            }
+            else{
+                return pokemon2;
+            }
+        }
+        else if(pokemon1.speed > pokemon2.speed){
             return pokemon1;
         }
         else {
